bdb0266b4e167a1feb9fd3c147877c018a11a89b
Jawad EL HATTAB <jawad.elhattab@hotmail.fr>
2022-01-19 09:21:59 +0100

    computed interval

--- app.js
+++ app.js
@@ -3,16 +3,29 @@ Vue.createApp({
         return {
             title: 'Vue<strong>Tracker</strong>',
             logoSource: './vue.svg',
+            tsFormatter: Intl.DateTimeFormat('fr', { hour: '2-digit', minute: '2-digit', }),
             tasks: [],
             taskID: 0,
             taskname: '',
             isTaskInProgress: false,
             startTime: null,
+            nowTime: null,
+            intervalEverySecond: null,
             errorMsg: null
         }
     },
-    methods: {
 
+    computed: {
+        currentDuration () {
+          if (this.startTime && this.nowTime) {
+            return this.durationBetweenTimestamps(this.startTime, this.nowTime)
+          } else {
+            return '00:00:00'
+          }
+        }
+    },
+
+    methods: {
         startTask() {
 
             // Vérification de la présence d'une tâche en cours
@@ -26,13 +39,17 @@ Vue.createApp({
             } else {
                 this.errorMsg = null;
             }
+            
             // Démarrage de la tâche
             this.isTaskInProgress = true
             this.startTime = Date.now()
+            this.nowTime = Date.now()
+            this.intervalEverySecond = setInterval(() => {
+                this.nowTime = Date.now()
+            }, 1000)
         },
 
         stopTask() {
-
             // Vérification de la présence d'une tâche en cours
             if (!this.isTaskInProgress) {
                 this.errorMsg = 'Aucune tâche n\'est en cours';
@@ -48,8 +65,10 @@ Vue.createApp({
             })
 
             // Fin de la tâche
+            clearInterval(this.intervalEverySecond)
             this.isTaskInProgress = false
             this.errorMsg = null
+            this.nowTime = null
             this.taskname = ''
         },
 
@@ -57,6 +76,21 @@ Vue.createApp({
 
             this.taskID++
             return this.taskID
+        },
+
+        formatTimestamp(ts) {
+            
+            return this.tsFormatter.format(ts)
+        },
+
+        durationBetweenTimestamps(start, end) {
+
+            let seconds = Math.floor((end / 1000) - (start / 1000))
+            let minutes = Math.floor(seconds / 60)
+            const hours = Math.floor(minutes / 60)
+            seconds = seconds % 60
+            minutes = minutes % 60
+            return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
         }
     }
 }).mount('#app')
